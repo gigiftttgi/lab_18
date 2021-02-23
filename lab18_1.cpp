b7b83906cec467b82374858351278538def737ed
NATKAMONLAK <paintlpch@gmail.com>
2021-02-23 16:06:59 +0700

    completed

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -5,8 +5,19 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect R1, Rect R2){
+
+	double x,y;
+    x = min(R1.x+R1.w,R2.x+R2.w) - max(R1.x,R2.x);
+    y = max(R1.y-R1.h,R2.y-R2.h) - min(R1.y,R2.y);
+    if(x*y > 0) return 0;
+    return -(x*y);
 
 
+}
 
+int main(){
+	Rect R1 = {1,1,5,5};
+    Rect R2 = {7,2,3,3};	
+    cout << overlap(R1,R2);	      
 }
